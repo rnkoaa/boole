f5ce319e50dda46b415be4c23ae2defddc015d15
rnkoaa <richard.agyei@gmail.com>
2015-11-30 23:29:55 -0600

    Accordion to the side of search results to help in filtering.

--- boole-web/src/main/resources/static/js/app/search/search.results.controller.js
+++ boole-web/src/main/resources/static/js/app/search/search.results.controller.js
@@ -4,6 +4,32 @@
 angular.module('booleApp')
     .controller('searchResultsController', ['$scope', '$log', 'searchService', '$state', '$stateParams',
         function ($scope, $log, searchService, $state, $stateParams) {
+            $scope.oneAtATime = false;
+
+            $scope.groups = [
+                {
+                    title: 'Dynamic Group Header - 1',
+                    content: 'Dynamic Group Body - 1'
+                },
+                {
+                    title: 'Dynamic Group Header - 2',
+                    content: 'Dynamic Group Body - 2'
+                }
+            ];
+
+            $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+            $scope.addItem = function() {
+                var newItemNo = $scope.items.length + 1;
+                $scope.items.push('Item ' + newItemNo);
+            };
+
+            $scope.status = {
+                isFirstOpen: true,
+                isFirstDisabled: false
+            };
+
+
             $scope.totalPages = 0;
             $scope.hasSearched = false;
             $scope.searchResults = null;
